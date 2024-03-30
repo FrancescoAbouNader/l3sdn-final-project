e5dd8d4b792d6e795c0efaf80e01b14a3ad4c672
Sepande <sepande.nivandy@gmail.com>
2024-03-30 12:26:08 +0100

    card

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,5 +1,43 @@
 <template>
-  <q-page class="flex flex-center"> </q-page>
+  <q-page class="flex flex-center">
+    <div class="q-pa-md row items-start q-gutter-md">
+      <q-card class="my-card">
+        <q-card-section class="bg-primary text-white">
+          <div class="text-h6">Nombre de managés</div>
+        </q-card-section>
+        <q-card-actions vertical align="center">
+          <q-btn flat>Voir</q-btn>
+        </q-card-actions>
+      </q-card>
+
+      <q-card class="my-card">
+        <q-card-section class="bg-purple text-white">
+          <div class="text-h6">Prochain entretien</div>
+        </q-card-section>
+        <q-card-actions vertical align="center">
+          <q-btn flat>Voir</q-btn>
+        </q-card-actions>
+      </q-card>
+
+      <q-card class="my-card">
+        <q-card-section class="bg-teal text-white">
+          <div class="text-h6">Mon manager</div>
+        </q-card-section>
+        <q-card-actions vertical align="center">
+          <q-btn flat>Voir</q-btn>
+        </q-card-actions>
+      </q-card>
+
+      <q-card class="my-card">
+        <q-card-section class="bg-grey-8 text-white">
+          <div class="text-h6">Mon prochain entretien personnel</div>
+        </q-card-section>
+        <q-card-actions vertical align="center">
+          <q-btn flat>Voir</q-btn>
+        </q-card-actions>
+      </q-card>
+    </div>
+  </q-page>
 </template>
 
 <script>
