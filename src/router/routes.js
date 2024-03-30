1bddb48d3edcb86c750f57e4b12376e6c0521de2
Sepande <sepande.nivandy@gmail.com>
2024-03-30 10:44:00 +0100

    o

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,11 @@
-
 const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: '/connexion', component: () => import('pages/ConnexionPage.vue') }
+      { path: '/connexion', component: () => import('pages/ConnexionPage.vue') },
+      { path: '/dashboard', component: () => import('pages/DashboardPage.vue') }
     ]
   },
 
